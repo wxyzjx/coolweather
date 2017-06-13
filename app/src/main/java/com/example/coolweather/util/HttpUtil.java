1e174fafe149d7528e04bd484eaad4d83d396a5d
wxyzjx <273857673@qq.com>
2017-06-13 09:29:21 +0800

    完成遍历省市县功能

---
+++ app/src/main/java/com/example/coolweather/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.example.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by wuxiangyu on 2017/6/12.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
